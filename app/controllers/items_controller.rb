2944fb5afd05a666ff2115ab0c9ad53cfc2c1dff
IWNFTH <taka2superhard@gmail.com>
2022-05-12 21:45:46 +0900

    出品ページへの遷移、プルダウン部分の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,33 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, only: :new
+
   def index
+    @item = Item.all
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :item_name ,:description, :category_id, :status_id, :shipping_charge_id, :shipping_area_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to user_session_path
+    end
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,27 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :shipping_charge
+  belongs_to :shipping_area
+  belongs_to :days_to_ship
   has_many :items
   has_many :buy_info
+  has_one_attached :image
+
+  validates :item_name,          presence: true
+  validates :description,        presence: true
+  validates :category_id,        presence: true
+  validates :status_id,          presence: true
+  validates :shipping_charge_id, presence: true
+  validates :shipping_area_id,   presence: true
+  validates :days_to_ship_id,    presence: true
+  validates :price,              presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_area_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 , message: "can't be blank" }
+
 end
