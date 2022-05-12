95122a3c3169864c369bef54cba8100471d46c8e
IWNFTH <taka2superhard@gmail.com>
2022-05-12 11:35:12 +0900

    userモデルの単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :first_name_kana, presence: true
-  validates :family_name, presence: true
-  validates :family_name_kana, presence: true
-  validates :birthday, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i #半角英数字混合のバリデーション
+  VALID_NAME_KANA = /\A[ァ-ヶー－]+\z/ #全角カタカナ表記のバリデーション
+  VALID_NAME = /\A[ぁ-んァ-ヶ一-龥々]+\z/ #全角（漢字・ひらがな・カタカナ）のバリデーション
+
+  validates :password,                         format: { with: VALID_PASSWORD_REGEX}
+  validates :nickname,         presence: true
+  validates :first_name,       presence: true, format: { with: VALID_NAME}
+  validates :first_name_kana,  presence: true, format: { with: VALID_NAME_KANA}
+  validates :family_name,      presence: true, format: { with: VALID_NAME}
+  validates :family_name_kana, presence: true, format: { with: VALID_NAME_KANA}
+  validates :birthday,         presence: true
 
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {Faker::Name.name}
+    email                 {Faker::Internet.free_email}
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation {password}
+    first_name            { '太郎' }
+    first_name_kana       { 'タロウ' }
+    family_name           { '佐藤' }
+    family_name_kana      { 'サトウ' }
+    birthday              { '2022-01-01' }
+  end
+ end
