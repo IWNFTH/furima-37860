d83a8d6251761bb026cb09bacc2a5b2627208b04
IWNFTH <taka2superhard@gmail.com>
2022-05-22 11:53:03 +0900

    Item:Order(1:1)のアソシエーション修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,7 @@ class OrdersController < ApplicationController
 
   def index
     @order_address = OrderAddress.new
-    redirect_to root_path if (current_user.id != @item.user_id) && @item.orders.present?
+    redirect_to root_path if (current_user.id != @item.user_id) && @item.order.present?
     redirect_to root_path if current_user.id == @item.user_id
   end
 
